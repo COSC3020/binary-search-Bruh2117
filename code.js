3ffa1c7631e493c19559c973b10d6d4eb0fd25d7
Aidan Giraldo <agirald1@uwyo.edu>
2023-09-20 20:47:12 -0600

    Update code.js
    Fails test, tried having the function take in the start and end indexes as arguments and would recursively call it, tried another way that would do the search in a while loop, but in any case it still mentions when failing the test that a counterexample to the function is the case of [1]. I would really appreciate some guidance on this. Thanks!

--- code.js
+++ code.js
@@ -1,11 +1,29 @@
 function binarySearch(list, element) {
+    if (list.length == 0)
+    return -1; 
+    else if (list.length == 1)
+    return list[0]; 
 
-    for (var i = 0; i < list.length; i++)
+    var left = 0 
+    var right = list.length - 1;
+
+    while (left <= right)
     {
-        if (list[i] == element)
+        var mid = Math.floor((left + right) / 2); 
+
+        if (list[mid] == element)
         {
-            return i; 
+            return element; 
+        }
+
+        if (list[mid] < element)
+        {
+            left = mid + 1; 
+        }
+        else if (list[mid] > element)
+        {
+            right = mid - 1; 
         }
     }
-    return -1;
+    return - 1; 
 }
